1b10643e8b11caa5194117d6b9be3bdcb5814298
rajeevg101 <72181625+rajeevg101@users.noreply.github.com>
2020-10-01 09:47:16 +0530

    Create powerpuffgirls.cpp

---
+++ powerpuffgirls.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+# define ll long long int
+using namespace std;
+
+void i_o_f()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    //input.txt = for input
+    freopen("input.txt", "r", stdin);
+    //output.txt = for output
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+int main()
+{
+    i_o_f();
+
+    ll n;
+    cin >> n;
+
+    ll *need = new ll[n];
+    for(int i=0; i<n; i++)
+        cin >> need[i];
+
+    ll *quty = new ll[n];
+    for(int i=0; i<n; i++)
+        cin >> quty[i];
+    
+    ll *outarr = new ll[n];
+    for(int i=0; i<n; i++)
+    {
+        outarr[i] = quty[i] / need[i];
+    }
+
+    sort(outarr, outarr + n);
+    for(int i=0; i<n; i++)
+        cout << outarr[i] << " ";
+
+
+    cout << endl << outarr[0];
+    
+    return 0;
+}
